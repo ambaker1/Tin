14adbb43dcae2399860af088e6c0066687428952
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-09 21:28:10 -0500

    no more pkgUnknown
    It is unworkable. Tabled for the future.
    Fixed some bugs and refactored a bit.

--- tests/tin_test.tcl
+++ tests/tin_test.tcl
@@ -1,13 +1,36 @@
 # First install
 cd ..
 source installer.tcl
+<<<<<<< Updated upstream
 # "Upgrade"
 package forget tin
 package require tin
 puts [tin install tin]
+=======
+# Upgrade
+# package forget tin
+# package require tin
+# puts [tin install tin]
+>>>>>>> Stashed changes
 # Importing commands
+# tin add foo 1.0 https://github.com/username/foo v1.0 install_foo.tcl
+# package require bar
+
 package forget tin
+<<<<<<< Updated upstream
 puts [package require tin]
+=======
+package require tin
+
+tin add foo 1.0 https://github.com/username/foo v1.0 install_foo.tcl
+catch {tin require foo 1.0} result; # will ask to install from github
+puts $result
+
+
+# package require bar
+
+
+>>>>>>> Stashed changes
 namespace eval ::foo {
     namespace export bar
 }
@@ -25,3 +48,8 @@ tin import -force import from tin 0.3
 
 import require from tin
 puts [require Tcl] 
+
+puts [package present tin]
+
+
+
