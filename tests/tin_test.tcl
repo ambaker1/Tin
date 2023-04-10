3f112c06ae0d23cb1a378de39934b54ce9e10fdf
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-09 21:29:24 -0500

    clean up

--- tests/tin_test.tcl
+++ tests/tin_test.tcl
@@ -1,36 +1,19 @@
 # First install
 cd ..
 source installer.tcl
-<<<<<<< Updated upstream
 # "Upgrade"
 package forget tin
 package require tin
 puts [tin install tin]
-=======
 # Upgrade
-# package forget tin
-# package require tin
-# puts [tin install tin]
->>>>>>> Stashed changes
-# Importing commands
-# tin add foo 1.0 https://github.com/username/foo v1.0 install_foo.tcl
-# package require bar
-
 package forget tin
-<<<<<<< Updated upstream
-puts [package require tin]
-=======
 package require tin
+puts [tin install tin]
 
 tin add foo 1.0 https://github.com/username/foo v1.0 install_foo.tcl
 catch {tin require foo 1.0} result; # will ask to install from github
 puts $result
 
-
-# package require bar
-
-
->>>>>>> Stashed changes
 namespace eval ::foo {
     namespace export bar
 }
