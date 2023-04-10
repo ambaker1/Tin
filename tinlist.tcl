4c4b4e1638c5d8e9e593bfc055105b31e2720ce5
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-09 23:15:34 -0500

    Update tinlist.tcl

--- tinlist.tcl
+++ tinlist.tcl
@@ -1,3 +1,2 @@
-tin add tin 0.3.2 https://github.com/ambaker1/Tin v0.3.2 installer.tcl
-tin add tin 0.3.3 https://github.com/ambaker1/Tin v0.3.3 installer.tcl
+tin add tin 0.3.4 https://github.com/ambaker1/Tin v0.3.4 installer.tcl
 tin add wob 0.1.2 https://github.com/ambaker1/wob v0.1.2 installer.tcl
