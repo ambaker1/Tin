49d7a539516c96cd70a759b6ef23232ef5065e18
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-11 16:17:19 -0500

    Overhauled how updates occur
    Now have a "fetch" command, which fetches version tags from compatible "auto" repositories.

--- tin.tcl
+++ tin.tcl
@@ -13,14 +13,15 @@
 namespace eval ::tin {
     # Internal variables
     variable tin ""; # Installation info for packages and versions
+    variable auto_tin ""; # Auto-update information for packages
     variable tinlib [file dirname [info library]]; # Default base directory
     
     # Exported commands (ensemble with "tin")
-    namespace export add auto_add remove; # Manipulate Tin database
+    namespace export add remove fetch; # Manipulate and Update Tin database
     namespace export packages versions repos; # Query Tin database
-    namespace export install update upgrade depend; # Install packages
+    namespace export install upgrade depend; # Install packages
     namespace export require import; # Package loading utilities
-    namespace export library mkdir build; # Package development utilities
+    namespace export library mkdir bake; # Package development utilities
     namespace ensemble create
 }
 
@@ -31,48 +32,35 @@ namespace eval ::tin {
 #
 # Add information for package and version for installation.
 #
+# Syntax:
+# tin add $name $version $repo $file $tag
+# tin add $name -auto $repo $file $tag $args...
+#
 # Arguments:
 # name          Package name
-# version       Package version
+# version       Package version (-auto to pull from GitHub tags)
 # repo          Github repository URL
-# tag           Github release tag
 # file          Installer .tcl file (relative to repo main folder)
+# args...       User-input package version requirements (see PkgRequirements)
 
-proc ::tin::add {name version repo tag file} {
+proc ::tin::add {name version repo file args} {
     variable tin
-    dict set tin $name $version $repo [list $tag $file]
-    return
-}
-
-# tin auto_add --
-#
-# Add directly from a GitHub repository using version number releases.
-# Must have prefix "v" and then the semantic versioning version number.
-# Release tag regex: ^v([0-9]+(\.[0-9]+)*([ab][0-9]+(\.[0-9]+)*)?)$
-#
-# Arguments:
-# name          Package name
-# repo          Repository associated with package
-# file          Installation file (must be the same for each release)
-
-proc ::tin::auto_add {name repo file} {
-    # Try to get the tags from repository using git
-    try {
-        set tags [exec git ls-remote --tags $repo]
-    } on error {errMsg options} {
-        # Re-raise the error (stripped of excess git stuff)
-        return -code error $errMsg
-    } on ok {result} {
-        # Trim root of git result
-        set tags [lmap {~ path} $result {file tail $path}]
+    variable auto_tin
+    # Auto-add configuration
+    if {$version eq "-auto"} {
+        # The package and repo are one. Installation files are linked to reqs
+        set reqs [PkgRequirements {*}$args]
+        dict set auto_tin $name repo $repo
+        dict set auto_tin $name reqs $file $reqs
+        return
     }
-    # Process the tag list
-    set exp {^v([0-9]+(\.[0-9]+)*([ab][0-9]+(\.[0-9]+)*)?)$}; # version regex
-    set tags [lsearch -inline -all -regexp $tags $exp]; # filter versions 
-    # Add to Tin database
-    foreach tag $tags {
-        set version [string range $tag 1 end]
-        tin add $name $version $repo $tag $file
+    # Single tag entry
+    if {[llength $args] == 1} {
+        set tag [lindex $args 0]
+        dict set tin $name $version $repo [list $file $tag]
+    } else {
+        return -code error \
+                "wrong # args: want \"tin add name version repo file tag\""
     }
     return
 }
@@ -80,51 +68,110 @@ proc ::tin::auto_add {name repo file} {
 # tin remove --
 #
 # Remove an entry from the tin database
+#
+# Syntax:
+# tin remove $name
+# tin remove $name -auto <$file>
+# tin remove $name $version <$repo>
 # 
 # Arguments:
-# name          Package name
-# version       Package version (optional, required with "repo")
-# repo          Package repository (optional)
+# name          Package name (required)
+# -auto         Option to remove auto configuration
+# file          Installer file for $name auto configuration (optional)
+# version       Package version to remove (mutually exclusive with -auto)
+# repo          Repository for $name $version
 
 proc ::tin::remove {name args} {
     variable tin
-    # tin remove $name
+    variable auto_tin
+    # Check arity for error
+    if {[llength $args] > 2} {
+        if {[lindex $args 0] eq "-auto"} {
+            return -code error \
+                    "wrong # args: want \"tin remove name -auto ?file?\""
+        }
+        return -code error \
+                "wrong # args: want \"tin remove name ?version? ?repo?\""
+    }
+    # Handle different removal cases
     if {[llength $args] == 0} {
+        # tin remove $name; # Remove all data for package "$name"
         dict unset tin $name
-        return
-    }
-    # tin remove $name $version
-    if {[llength $args] == 1} {
-        set version [lindex $args 0]
-        if {[dict exists $tin $name]} {
-            dict unset tin $name $version
+        dict unset auto_tin $name
+    } elseif {[lindex $args 0] eq "-auto"} {
+        if {![dict exists $auto_tin $name]} {
+            return
         }
-        return
-    }
-    # tin remove $name $version $repo
-    if {[llength $args] == 2} {
-        lassign $args version repo
-        if {[dict exists $tin $name $version]} {
-            dict unset tin $name $version $repo
+        if {[llength $args] == 1} {
+            # tin remove $name -auto; # Remove all auto-tin data
+            dict unset auto_tin $name
+        } else {
+            # tin remove $name -auto $file; # Remove specific auto-tin data
+            dict unset auto_tin $name reqs $file
         }
-        return
+    } else {
+        # Remove only tin data
+        if {[dict exists $tin $name {*}$args]} {
+            dict unset tin {*}$args
+        }
     }
-    return -code error "wrong # args: want \"tin remove name ?version? ?repo?\""
+    return
 }
 
-# Query Tin database
+# tin fetch --
+#
+# Fetch version numbers from a GitHub repository
+# Add directly from a GitHub repository using version number releases.
+# Must have prefix "v" and then the semantic versioning version number.
+# Release tag regex: ^v([0-9]+(\.[0-9]+)*([ab][0-9]+(\.[0-9]+)*)?)$
+#
+# Arguments:
+# pattern       Package name pattern (default *)
+
+proc ::tin::fetch {{pattern *}} {
+    variable auto_tin
+    # Fetch version tags (using regexp), and add to Tin
+    set exp {^v([0-9]+(\.[0-9]+)*([ab][0-9]+(\.[0-9]+)*)?)$}
+    dict for {name data} [dict filter $auto_tin key $pattern] {
+        # Define the regular expression for getting version tags
+        try {
+            exec git ls-remote --tags [dict get $data repo]
+        } on error {errMsg options} {
+            # Raise warning, but do not throw error.
+            puts "warning: failed to fetch tags for $name at $repo"
+            puts $errMsg
+        } on ok {result} {
+            # Acquired tag data. Strip excess data, and filter for regexp
+            set tags [lmap {~ path} $result {file tail $path}]
+            set tags [lsearch -inline -all -regexp $tags $exp]
+            # Loop through tags, and add to Tin if within specified reqs
+            foreach tag $tags {
+                set version [string range $tag 1 end]
+                dict for {file reqs} [dict get $data reqs] {
+                    if {[package vsatisfies $version {*}$reqs]} {
+                        tin add $name $version $repo $file $tag
+                    }
+                }
+            }; # end foreach tag
+        }; # end try
+    }; # end dict for 
+    return
+}
+
+# Query Tin database (not auto_tin)
 ################################################################################
 
 # tin packages --
 #
-# Get list of available tin packages, using glob pattern
+# Get list of available tin packages, including auto_tin, using glob pattern
 # 
 # Arguments:
-# pattern           "glob" pattern
+# pattern           "glob" pattern for matching against package names
 
 proc ::tin::packages {{pattern *}} {
     variable tin
-    dict keys $tin $pattern
+    variable auto_tin
+    concat [dict keys $tin $pattern] [dict keys $auto_tin $pattern]
 }
 
 # tin versions --
@@ -141,7 +188,7 @@ proc ::tin::versions {name args} {
         return
     }
     # Get sorted list (ascending) of versions
-    set versions [dict keys [dict get $tin $name tags]]
+    set versions [dict keys [dict get $tin $name]]
     # Filter for version requirements
     if {[llength $args]} {
         set versions [FilterVersions $versions [PkgRequirements {*}$args]]
@@ -160,10 +207,10 @@ proc ::tin::versions {name args} {
 
 proc ::tin::repos {name version} {
     variable tin
-    if {![dict exists $tin $name $version]} {
-        return ""
+    if {[dict exists $tin $name $version]} {
+        return [dict keys [dict get $tin $name $version]]
     }
-    dict keys [dict get $tin $name $version]
+    return
 }
 
 # Installing and updating packages
@@ -179,6 +226,7 @@ proc ::tin::repos {name version} {
 
 proc ::tin::install {name args} {
     variable tin
+    variable auto_tin
     puts "searching in Tin database for $name $args ..."
     if {![dict exists $tin $name]} {
         return -code error "can't find $name in Tin database"
@@ -189,65 +237,50 @@ proc ::tin::install {name args} {
     if {$version eq ""} {
         return -code error "can't find $name $args in Tin database"
     }
-    puts "found $name $version in Tin database"
     
-    # Search for valid repository (allow for multiple repos)
-    set ok 0; # boolean check variable
+    # Loop through repositories for selected version
     dict for {repo data} [dict get $tin $name $version] {
-        lassign $data tag installer
-        puts "attempting to access $repo to install $name $version ..."
-        try { 
-            # Get list of tags from the repository
-            exec git ls-remote --tags $repo
-        } on error {errMsg options} {
-            # Relay error message to screen
-            puts $errMsg
-            continue
-        } on ok {result} { 
-            # Check if tag exists in repository
-            set tags [lmap {~ path} $result {file tail $path}]
-            if {$tag in $tags} {
-                set ok 1
-                break
+        lassign $data file tag
+        try {
+            # Try to clone the repository into a temporary directory
+            close [file tempfile temp]
+            file delete $temp
+            file mkdir $temp
+            try {
+                exec git clone --depth 1 --branch $tag $repo $temp
+            } on error {result options} {
+                if {[dict get $options -errorcode] ne "NONE"} {
+                    return -code error $result
+                }
             }
-        }
-    }
-    if {!$ok} {
-        return -code error "no valid repository found for $name $version"
-    }
-    
-    # Clone the repository into a temporary directory
-    puts "installing $name $version from $repo $tag ..."
-    close [file tempfile temp]
-    file delete $temp
-    file mkdir $temp
-    try {
-        exec git clone --depth 1 --branch $tag $repo $temp
-    } on error {result options} {
-        if {[dict get $options -errorcode] ne "NONE"} {
-            return -code error $result
-        }
-    }
 
-    # Extract the package from the cloned repository, in fresh interpreter
-    set home [pwd]
-    cd $temp
-    set child [interp create]
-    if {[catch {$child eval [list source $installer]} result options]} {
-        puts "error in running installer file"
-        return -options $options $result
-    }
-    interp delete $child
-    cd $home
-    file delete -force $temp
+            # Install package from the cloned repository, in a fresh interpreter
+            puts "installing $name $version from $repo $tag ..."
+            set home [pwd]
+            cd $temp
+            set child [interp create]
+            if {[catch {$child eval [list source $file]} result options]} {
+                puts "error in running installer file"
+                return -options $options $result
+            }
+            interp delete $child
+            cd $home
+            file delete -force $temp
 
-    # Check for proper installation and return version 
-    if {![PkgInstalled $name $reqs]} {
-        puts "$name version $version installed successfully"
+            # Check for proper installation and return version 
+            if {![PkgInstalled $name $version-$version]} {
+                puts "$name version $version installed successfully"
+            } else {
+                return -code error "failed to install $name version $version"
+            }
+        } on error {errMsg options} {
+            # Catch the error, and try another repository (if any)
+            continue
+        }
         return $version
-    } else {
-        return -code error "failed to install $name version $version"
     }
+    # Re-raise the error in the "try" block
+    return -options $options $errMsg
 }
 
 # tin upgrade --
@@ -279,25 +312,6 @@ proc ::tin::upgrade {name args} {
     puts "upgrading $name v$installed to v$version ..."
     tin install $name -exact $version
     return $version
-}
-
-# tin update --
-#
-# Update Tin from https://github.com/ambaker1/Tin
-# Performs minor and patch version upgrades, which updates the tinlist.
-# Will not upgrade a major version, must do that manually.
-#
-# How to do a major version update (example from 1.0 to 2.0):
-# ------------------------------------------------------------------------------
-# package require tin 1.0
-# tin update; # Updates Tin database
-# tin require tin 2.0; # Installs with new major number
-# ------------------------------------------------------------------------------
-
-proc ::tin::update {} {
-    tin auto_add tin https://github.com/ambaker1/Tin install.tcl
-    set version [tin upgrade tin [package present tin]]
-    tin require tin -exact $version
 }
 
 # tin depend --
@@ -445,16 +459,17 @@ proc ::tin::mkdir {args} {
     return $dir
 }
 
-# tin build --
+# tin bake --
 #
 # Perform substitution on file contents and write to new files
+# Allows for uppercase alphanum variable substitution (e.g. @VARIABLE@)
 #
 # Arguments:
 # inFile        File to read (with @VARIABLE@ declarations)
 # outFile       File to write to after substitution
 # config        Dictionary with keys of config variable names, and values.
 
-proc ::tin::build {inFile outFile config} {
+proc ::tin::bake {inFile outFile config} {
     # Get string map for config variable names (must be uppercase alphanum)
     set mapping ""
     dict for {key value} $config {
--- tinlist.tcl
+++ tinlist.tcl
@@ -1,1 +1,1 @@
-tin add tin 0.4.0 https://github.com/ambaker1/Tin v0.4.0 install.tcl
+tin add tin -auto https://github.com/ambaker1/Tin install.tcl 0.4.0
