6e53ea442df6f66acabccbf35949b05095029d02
Alex Baker <ambaker1@mtu.edu>
2023-04-19 14:31:54 -0500

    Delete make.tcl

--- make.tcl
+++
@@ -1,7 +0,0 @@
-source tin.tcl
-dict set config LIBRARY tin0.4
-dict set config VERSION 0.4.0
-tin build src/tin.tin tin.tcl $config
-tin build src/tinlist.tin tinlist.tcl $config
-tin build src/pkgIndex.tin pkgIndex.tcl $config
-tin build src/install.tin install.tcl $config
