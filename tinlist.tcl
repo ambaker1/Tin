18c2d1ec01bf100f2f0b1f2267de5ec39f681134
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-04-25 14:40:04 -0500

    Added wob

--- tinlist.tcl
+++ tinlist.tcl
@@ -1,2 +1,3 @@
 tin add -auto tin https://github.com/ambaker1/Tin install.tcl 0
 tin add -auto tintest https://github.com/ambaker1/Tin-Test install.tcl
+tin add -auto wob https://github.com/ambaker1/wob install.tcl 0.1.3-
