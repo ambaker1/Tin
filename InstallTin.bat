2aedefdd3bbc50779f685687fe14c7ed62e4ef6f
Alex Baker <ambaker1@mtu.edu>
2023-05-08 14:28:54 -0500

    Update InstallTin.bat
    Tested, handles errors now

--- InstallTin.bat
+++ InstallTin.bat
@@ -1,5 +1,11 @@
 @echo off
 cd "%~dp0"
-tclsh install.tcl
-echo Tin is now installed :)
+tclsh install.tcl || goto :error
+echo Tin is now installed :) 
 pause
+goto :EOF
+
+:error
+echo Failed to install Tin :(
+pause
+exit /b %errorlevel%
